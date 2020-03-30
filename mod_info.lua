8dc484e6cf05fcd0d9c71f11440fdbba65145fc0
CDRMV <46399086+CDRMV@users.noreply.github.com>
2020-03-30 18:35:29 +0200

    Add Icon + Modinfo

---
+++ mod_info.lua
@@ -0,0 +1,17 @@
+name = "(F.B.P.) Future Battlefield Pack: Legends"
+uid = "5t3edt-btz6-9438-h6ui-967gt56fa1"
+version = 1.0
+copyright = "Copyright © 2020, CDRMV (aka CDR Deadalus)"
+description = "'Future Battlefield Pack Legends' is a Unitpack for Supreme Commander Forged Alliance. It adds adds several new Units based on official GPG Concept Artwork as recreations into the Game."
+author = "CDRMV (aka CDR Deadalus)"
+url = "http://www.moddb.com/mods/fbp-future-battlefield-pack"
+icon = "/mods/Future Battlefield Pack Legends/Icon.png" 
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = { }
+requiresNames = { }
+conflicts = { }
+before = { }
+after = { }
