7bf13333acc0737c47b5d82c280aae8c94b1810d
CDRMV <46399086+CDRMV@users.noreply.github.com>
2024-01-02 18:48:39 +0100

    Updates the Mod Version from 1 to 3

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "(F.B.P.) Future Battlefield Pack: Legends"
 uid = "5t3edt-btz6-9438-h6ui-967gt56faL1"
-version = 1.0
+version = 3.0
 copyright = "Copyright © 2021, CDRMV (aka CDR Deadalus)"
 description = "'Future Battlefield Pack Legends' is a Unitpack for Supreme Commander Forged Alliance. It adds adds several new Units based on official GPG Concept Artwork as recreations into the Game."
 author = "CDRMV (aka CDR Deadalus)"
