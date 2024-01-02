812c947524d44587805d762ae722a8b27227d1b2
CDRMV <46399086+CDRMV@users.noreply.github.com>
2024-01-02 19:57:19 +0100

    Updates the Mod Version from 1 to 3

--- mod_info.lua
+++ mod_info.lua
@@ -1,9 +1,9 @@
 name = "(F.B.P.) Future Battlefield Pack: Legends"
-uid = "5t3edt-btz6-9438-h6ui-967gt56faL1"
-version = 3.0
-copyright = "Copyright © 2021, CDRMV (aka CDR Deadalus)"
-description = "'Future Battlefield Pack Legends' is a Unitpack for Supreme Commander Forged Alliance. It adds adds several new Units based on official GPG Concept Artwork as recreations into the Game."
-author = "CDRMV (aka CDR Deadalus)"
+uid = "5t3edt-btz6-9438-h6ui-967gt56faL13"
+version = 5
+copyright = "Copyright © 2024, CDRMV"
+description = "'Future Battlefield Pack Legends' is a Unitpack for Supreme Commander Forged Alliance. It adds adds several new Units based on official GPG Concept Artwork as recreations into the Game. This is an Patch, which fixes several Issues in FAF to make the Mod playable. Keep in Mind this is still an WIP Mod and the Units will get an full Quality Rework in the Future with better looking Models and Textures."
+author = "CDRMV"
 url = "http://www.moddb.com/mods/fbp-future-battlefield-pack"
 icon = "/mods/Future Battlefield Pack Legends/Icon.png" 
 selectable = true
